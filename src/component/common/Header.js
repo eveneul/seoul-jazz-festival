668195d46d5cb27f3ee520875cfa91d0325566b9
오하늘 <nebulass@naver.com>
2022-06-09 04:45:58 +0900

    Header JSX 추가 / 스타일 추가

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Menu from './Menu';
 
-function Header() {
-	return <div>Header</div>;
+function Header({ type }) {
+	const [MenuBtn, setMenuBtn] = useState(false);
+	const onMenu = () => {
+		setMenuBtn(!MenuBtn);
+	};
+	return (
+		<>
+			{MenuBtn ? <Menu onMenu={onMenu} /> : null}
+			<header className={type}>
+				<div className='inner'>
+					<h1>
+						<Link to='/'>
+							<img
+								src={`${process.env.PUBLIC_URL}/img/logo-white.png`}
+								alt='Seoul jazz festival Logo'
+							/>
+						</Link>
+					</h1>
+
+					<Link
+						to='/'
+						className='menuBtn'
+						onClick={() => {
+							onMenu();
+						}}>
+						<span>매뉴 호출 버튼</span>
+					</Link>
+				</div>
+			</header>
+		</>
+	);
 }
 
 export default Header;

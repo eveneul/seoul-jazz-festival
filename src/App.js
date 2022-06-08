a41156c8457e95823744220a370dea35fd0c7e4f
오하늘 <nebulass@naver.com>
2022-06-09 04:45:35 +0900

    기본적인 레이아웃 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
+import { Route, Switch } from 'react-router-dom';
+import './scss/style.scss';
+
+//common
+import Header from './component/common/Header';
+import Footer from './component/common/Footer';
+
+//main
+import Visual from './component/main/Visual';
+//sub
+import Artist from './component/sub/Artist';
+import Faq from './component/sub/Faq';
+import Gallery from './component/sub/Gallery';
+import Join from './component/sub/Join';
+import Notice from './component/sub/Notice';
+import Youtube from './component/sub/Youtube';
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<Switch>
+				<Route exact path='/'>
+					<Header type={'main'} />
+					<Visual />
+				</Route>
+
+				<Route path='/' render={() => <Header type={'sub'} />} />
+			</Switch>
+
+			<Route path='/artist' component={Artist} />
+			<Route path='/faq' component={Faq} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/join' component={Join} />
+			<Route path='/notice' component={Notice} />
+			<Route path='/youtube' component={Youtube} />
+		</>
 	);
 }
 

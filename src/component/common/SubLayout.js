8d84042ca3b2387b9f9ef1e18e904c84902dad5b
오하늘 <nebulass@naver.com>
2022-06-09 04:46:50 +0900

    서브 페이지 레이아웃 생성

--- src/component/common/SubLayout.js
+++ src/component/common/SubLayout.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useRef, useEffect } from 'react';
 
-function SubLayout() {
-	return <div>SubLayout</div>;
+function SubLayout({ name, children }) {
+	const frame = useRef(null);
+	return (
+		<>
+			<section className={`content ${name}`} ref={frame}>
+				<div className='title'>
+					<h2>{name}</h2>
+				</div>
+				<div className='inner'>{children}</div>
+			</section>
+		</>
+	);
 }
 
 export default SubLayout;

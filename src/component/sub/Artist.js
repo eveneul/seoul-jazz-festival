51c790290571400324f90d9e2da7a47aa568911a
오하늘 <nebulass@naver.com>
2022-06-16 00:24:22 +0900

    store에 담긴 값 useSelector로 호출

--- src/component/sub/Artist.js
+++ src/component/sub/Artist.js
@@ -4,20 +4,16 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Popup from '../common/Popup';
 import Timetable from '../common/Timetable';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Artist() {
 	const path = process.env.PUBLIC_URL;
-	const [artists, setArtists] = useState([]);
 	const [onPopup, setOnPopup] = useState(false);
 	const togglePop = () => {
 		setOnPopup(true);
 	};
 
-	useEffect(() => {
-		axios.get(`${path}/DB/artist.json`).then((json) => {
-			setArtists(json.data.artist);
-		});
-	}, []);
+	const Artist = useSelector((store) => store.artistReducer.artist);
 
 	return (
 		<>
@@ -25,13 +21,13 @@ function Artist() {
 			<SubLayout name={'Artist'} color={'#000'}>
 				<article className='artist-list'>
 					<ul>
-						{artists.map((artist, idx) => {
+						{Artist.map((artist, idx) => {
 							return <li key={idx}>{artist.name}</li>;
 						})}
 					</ul>
 				</article>
 				<article className='artist-thumb'>
-					{artists.map((artist, idx) => {
+					{Artist.map((artist, idx) => {
 						return (
 							<div
 								className='artist-card'

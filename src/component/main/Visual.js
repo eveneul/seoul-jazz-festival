bc5093ebd1a5b37110d1d39847d9608d2afd86ff
오하늘 <nebulass@naver.com>
2022-06-15 18:48:39 +0900

    main page visual section 수정

--- src/component/main/Visual.js
+++ src/component/main/Visual.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDownLong } from '@fortawesome/free-solid-svg-icons';
 
 function Visual() {
-	return <figure className='visual'>Visual</figure>;
+	return (
+		<figure className='visual'>
+			<div className='text'>
+				<div className='title'>
+					<h2>
+						seoul <br /> jazz <br /> festival
+					</h2>
+				</div>
+				<div className='sub-title'>
+					<span>Fri, May 27 - Sun, May 29</span>
+					<span>88 Garden Olympic Park</span>
+				</div>
+			</div>
+			<div className='vid'>
+				<div></div>
+			</div>
+			<div className='scroll-icon'>
+				<FontAwesomeIcon icon={faArrowDownLong} />
+			</div>
+		</figure>
+	);
 }
 
 export default Visual;

28df80d18a6e31560eabe16e672981533f5f8a84
오하늘 <nebulass@naver.com>
2022-06-20 01:59:01 +0900

    redux로 메인페이지에 youtube api 호출

--- src/component/main/Vid.js
+++ src/component/main/Vid.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import { useRef, useState } from 'react';
+import BasicPop from '../sub/Youtube';
 
 function Vid() {
-	return <figure className='Vid'>Vid</figure>;
+	const Vids = useSelector((store) => store.youtube.data);
+	const [idx, setIdx] = useState(0);
+	const [open, setOpen] = useState(false);
+	const pop = useRef(null);
+
+	return (
+		<>
+			<figure className='Vid'>
+				<div className='inner'>
+					{Vids.map((vid, idx) => {
+						if (idx < 4) {
+							return (
+								<article key={idx}>
+									<div
+										className='pic'
+										onClick={() => {
+											setIdx(idx);
+											pop.current.open();
+										}}>
+										<img
+											src={vid.snippet.thumbnails.standard.url}
+											alt={vid.snippet.title}
+										/>
+									</div>
+								</article>
+							);
+						}
+					})}
+				</div>
+			</figure>
+		</>
+	);
 }
 
 export default Vid;

25d2af2aea426c4a3da420d5860ea9d8c95f6745
오하늘 <nebulass@naver.com>
2022-06-21 01:41:20 +0900

    main page vid content 수정

--- src/component/main/Vid.js
+++ src/component/main/Vid.js
@@ -2,15 +2,19 @@ import { useSelector } from 'react-redux';
 import { useRef, useState } from 'react';
 import BasicPop from '../sub/Youtube';
 
-function Vid() {
+function Vid({ scroll, start, base }) {
 	const Vids = useSelector((store) => store.youtube.data);
 	const [idx, setIdx] = useState(0);
 	const [open, setOpen] = useState(false);
 	const pop = useRef(null);
+	let position = 0;
+	if (start) position = scroll - start - base;
 
 	return (
 		<>
-			<figure className='Vid'>
+			<figure className='Vid mainSelection'>
+				<h3 style={{ left: 100 + position }}>seoul jazz festival</h3>
+				<span style={{ right: 100 + position }}>Fri, May 27 - Sun, May 29</span>
 				<div className='inner'>
 					{Vids.map((vid, idx) => {
 						if (idx < 4) {
@@ -26,9 +30,6 @@ function Vid() {
 											src={vid.snippet.thumbnails.standard.url}
 											alt={vid.snippet.title}
 										/>
-									</div>
-									<div className='btn'>
-										<span>Click!</span>
 									</div>
 								</article>
 							);
